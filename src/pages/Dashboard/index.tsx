a6e82877dc84d397bba50beebd7a758eeac8cd71
Rodrigo Brochado <rodbove@gmail.com>
2020-07-16 10:41:42 -0300

    added api service

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,67 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="logo" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/41601403?s=460&u=28eb9fe563ea0b3f6a320d8055d7181670898797&v=4"
-            alt="Rodrigo"
-          />
-          <div>
-            <strong>poowoox/github-explorer</strong>
-            <p>
-              A simple and clean ReactJS application for exploring Github
-              repositories.
-            </p>
-          </div>
+        {repositories.map(repository => (
+          <a href="teste" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/41601403?s=460&u=28eb9fe563ea0b3f6a320d8055d7181670898797&v=4"
-            alt="Rodrigo"
-          />
-          <div>
-            <strong>poowoox/github-explorer</strong>
-            <p>
-              A simple and clean ReactJS application for exploring Github
-              repositories.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/41601403?s=460&u=28eb9fe563ea0b3f6a320d8055d7181670898797&v=4"
-            alt="Rodrigo"
-          />
-          <div>
-            <strong>poowoox/github-explorer</strong>
-            <p>
-              A simple and clean ReactJS application for exploring Github
-              repositories.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
